a32010321fd5edfbd5e75067377186bb7153df79
Jasontang98 <jasontang@DESKTOP-K3NMI40.localdomain>
2022-06-24 14:59:58 -0700

    aws and songs route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-
+const songsRouter = require("./songs.js");
 
 
 const sessionRouter = require('./session.js');
@@ -8,6 +8,7 @@ const usersRouter = require('./users.js');
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
+router.use("/song", songsRouter)
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
